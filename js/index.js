51fb0c5938aa59d70ff5906ea65c0d75ef43f6c6
Dan Sample <danssample@gmail.com>
2019-04-13 11:47:08 -0700

    built out 2 event listeners for 2 middle images

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+//Changes the color of the the nav items if mouse is hovered over them, and reverts color back when not.
+
 let navItems = document.querySelectorAll('.nav-link');
 navItems.forEach(function(item){
     item.addEventListener('mouseover', function(event){
@@ -9,6 +12,8 @@ navItems.forEach(function(item){
     })
 })
 
+// inverts the images color when mouse clicked, and reverts when mouse click depressed.
+
 let funBus = document.querySelector('.busImg');
 funBus.addEventListener('mousedown', function(event){
     event.target.classList.toggle('img-invert')
@@ -16,4 +21,30 @@ funBus.addEventListener('mousedown', function(event){
 funBus.addEventListener('mouseup', function(event){
     event.target.classList.toggle('img-invert')
 })
+funBus.addEventListener('mouseout', function(event){
+    event.target.classList.remove('img-invert')
+})
 
+// Switches the images when single mouse clicked, and switches it back on double mouse click.
+
+let funMap = document.querySelector('.mapImg');
+funMap.addEventListener('click', function(event){
+    event.target.setAttribute('src','img/fun.jpg')
+})
+funMap.addEventListener('dblclick', function(event){
+    event.target.setAttribute('src', 'img/adventure.jpg')
+})
+
+// Switches the images when single mouse clicked, and switches it back on double mouse click.
+
+let funAdventure = document.querySelector('.img-fluid');
+funAdventure.addEventListener('click', function(event){
+    event.target.setAttribute('src','img/adventure.jpg')
+})
+funAdventure.addEventListener('dblclick', function(event){
+    event.target.setAttribute('src', 'img/fun.jpg')
+})
+
+// 
+
+let

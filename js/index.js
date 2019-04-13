025e3e32ba6ca4f045a664c654d86d2c4610f9cb
Dan Sample <danssample@gmail.com>
2019-04-13 10:49:39 -0700

    built out 4 event listeners for item items and top img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+let navItems = document.querySelectorAll('.nav-link');
+navItems.forEach(function(item){
+    item.addEventListener('mouseover', function(event){
+        event.target.style.color = '#17A2B8';
+    })
+    item.addEventListener('mouseout', function(event){
+        event.target.style.color = '#212529';
+    })
+})
+
+let funBus = document.querySelector('.busImg');
+funBus.addEventListener('mousedown', function(event){
+    event.target.classList.toggle('img-invert')
+})
+funBus.addEventListener('mouseup', function(event){
+    event.target.classList.toggle('img-invert')
+})
+
